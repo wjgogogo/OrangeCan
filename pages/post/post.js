5b97474308decd841ff2ce4ee87ffb21e9e1b1ca
AHOhhhh <906626481@qq.com>
2018-09-25 09:28:47 +0800

    [WJ]feat: add post data

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,1 +1,27 @@
-Page({})
+const data = require("../../data/postData.js");
+Page({
+    data: {
+        title: "post"
+    },
+
+    onLoad: function (options) {
+        console.log("onLoad");
+        this.setData({postList: data.postList});
+    },
+
+    onReady: function () {
+        console.log("onReady")
+    },
+
+    onShow: function () {
+        console.log("onShow")
+    },
+
+    onHide: function () {
+        console.log("onHide")
+    },
+
+    onUnload: function () {
+        console.log("onUnload")
+    }
+});
