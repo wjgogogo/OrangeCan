30c721095113c30459f0e04e710d38f2b94c8ac6
AHOhhhh <906626481@qq.com>
2018-09-25 12:43:16 +0800

    [WJ]feat: add jump page function

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,1 +1,24 @@
-Page({})
+Page({
+    onTapJump: function (event) {
+        wx.redirectTo({
+            url: "../post/post",
+            success: function () {
+                console.log("success")
+            },
+            fail: function () {
+                console.log("fail")
+            },
+            complete: function () {
+                console.log("complete")
+            }
+        })
+    },
+
+    onHide:function () {
+        console.log("welcome hide");
+    },
+
+    onUnload:function () {
+        console.log("welcome unload");
+    }
+})
