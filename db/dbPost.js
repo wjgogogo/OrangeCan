7f461f7b0cae6109146ceb40d09b00b6d1b17ff3
AHOhhhh <906626481@qq.com>
2018-09-25 23:37:09 +0800

    [WJ]feat: add post detail page

--- db/dbPost.js
+++ db/dbPost.js
@@ -1,6 +1,7 @@
 class DBPost {
-    constructor() {
+    constructor(postId = 0) {
         this.storageKeyName = 'postList';
+        this.postId = postId;
     }
 
     getAllPostData() {
@@ -15,6 +16,15 @@ class DBPost {
     execSetStorageSync(data) {
         wx.setStorageSync(this.storageKeyName, data);
     }
+
+    getPostItemById() {
+        const dataList = this.getAllPostData();
+        const index = dataList.findIndex(data => data.postId == this.postId);
+        return {
+            index,
+            data: dataList[index]
+        }
+    }
 }
 
-export  default DBPost;
+export default DBPost;
--- pages/post/post-detail/post-detail.js
+++ pages/post/post-detail/post-detail.js
@@ -1,3 +1,13 @@
-Page({
+import DBPost from "../../../db/dbPost";
 
+Page({
+    onLoad: function (options) {
+        const postId = options.id;
+        this.dbPostData = new DBPost(postId).getPostItemById();
+        this.setData({...this.dbPostData})
+    },
+
+    onReady: function () {
+        wx.setNavigationBarTitle({title: this.data.data.title})
+    }
 });
