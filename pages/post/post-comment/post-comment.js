4ccbbcc354a477653defbc867fdcd75547b9f2d5
AHOhhhh <906626481@qq.com>
2018-09-26 23:34:55 +0800

    [WJ]feat: add img preview

--- pages/post/post-comment/post-comment.js
+++ pages/post/post-comment/post-comment.js
@@ -6,6 +6,16 @@ Page({
         this.dbPost = new DBPost(postId);
         this.setData({comments:this.dbPost.getCommentData()})
     },
+    previewImg:function (event) {
+        const commentIdx=event.currentTarget.dataset.commentIdx;
+        const imgIdx=event.currentTarget.dataset.imgIdx;
+        const imgs=this.data.comments[commentIdx].content.img;
+        console.log(commentIdx,imgIdx,imgs)
+        wx.previewImage({
+            urls:imgs,
+            current:imgs[imgIdx]
+        })
+    }
 
 
 });
